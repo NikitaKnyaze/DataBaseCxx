3d3f9348242e79090abe83780133704331b262f9
Knyazev Nikita <nk8222138@gmail.com>
2024-07-30 22:22:23 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <pqxx/pqxx>
 #include <Windows.h>
 #include <tuple>
+#include <exception>
 
 class clientbd
 {
@@ -106,29 +107,37 @@ int main()
 	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
 	setlocale(LC_ALL, "Russian");
+	
+	try 
+	{
+		std::string connectionString = "host=127.0.0.1 port=5432 dbname=DataBaseCxx user=postgres password=1209R";
+		clientbd manager(connectionString);
 
-	std::string connectionString = "host=127.0.0.1 port=5432 dbname=DataBaseCxx user=postgres password=1209R";
-	clientbd manager(connectionString);
+		manager.clear_clients_table(); // удаление табл
+		manager.clear_clients_phones(); // удаление табл
+		manager.add_new_client("Alex", "Stuart", "AS@gmail.com"); // 1 добавление клиента
+		manager.add_new_client("Max", "Power", "MP@gmail.com"); // 1 добавление клиента
 
-	manager.clear_clients_table(); // удаление табл
-	manager.clear_clients_phones(); // удаление табл
-   manager.add_new_client("Alex", "Stuart", "AS@gmail.com"); // 1 добавление клиента
-	manager.add_new_client("Max", "Power", "MP@gmail.com"); // 1 добавление клиента
+		manager.add_telephone("+7 999 568-543-33", "1"); // 2 добавление номера к сущ-у клиенту 
+		manager.add_telephone("+7 999 990-99-99", "2");// 2 добавление номера к сущ-у клиенту 
 
-   manager.add_telephone("+7 999 568-543-33", "1"); // 2 добавление номера к сущ-у клиенту 
-	manager.add_telephone("+7 999 990-99-99", "2");// 2 добавление номера к сущ-у клиенту 
+		manager.change_inform("1", "Alex", "Stuart", "AlexStuart@gmail.com"); // 3 смена информации об клиенте
 
-   manager.change_inform("1", "Alex", "Stuart", "AlexStuart@gmail.com"); // 3 смена информации об клиенте
+		manager.add_telephone("+7 999 534-333-21", "1"); // 4 добавление номера к сущ-у клиенту
 
-   manager.add_telephone("+7 999 534-333-21", "1"); // 4 добавление номера к сущ-у клиенту
+		manager.delete_phone("+7 999 568-543-33", "1"); // 5 удаление номера телефона
 
-   manager.delete_phone("+7 999 568-543-33","1"); // 5 удаление номера телефона
+		manager.delete_client("2"); // удаление клиенты из БД
 
-	manager.delete_client("2"); // удаление клиенты из БД
-
-	manager.find_client("SELECT c.client_id, c.first_name, c.last_name, c.email, p.phone_number "
-						"FROM clients c LEFT JOIN phones p ON c.client_id = p.client_id "
-		"WHERE c.first_name = 'Alex'");
+		manager.find_client
+		(  "SELECT c.client_id, c.first_name, c.last_name, c.email, p.phone_number "
+			"FROM clients c LEFT JOIN phones p ON c.client_id = p.client_id "
+			"WHERE c.first_name = 'Alex'");
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Exception happened: " << e.what() << std::endl;
+	}
 
 	return 0;
 }
